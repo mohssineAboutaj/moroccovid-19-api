f76bcb91b32c119d065df4ced92330308f56799c
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2021-02-26 17:58:32 +0100

    remove non-required dependencies

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // import dependencies
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const { get } = require("axios");
 
 const sourceURL = "https://disease.sh/v3/covid-19/countries/morocco";
 const port = process.env.PORT || 2019;
@@ -10,17 +8,12 @@ const port = process.env.PORT || 2019;
 // init app
 const app = express();
 
-// use some middleware
-app.use(cors());
-app.use(bodyParser.json());
-
 // init express routing
 const router = express.Router();
 
 // define routes
 router.get("/", (req, res) => {
-  axios
-    .get(sourceURL)
+  get(sourceURL)
     .then(({ data }) => {
       res.json(data);
     })
