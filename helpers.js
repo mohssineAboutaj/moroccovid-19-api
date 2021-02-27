1b0a27d9d814bfc9b9c42864d530d52ea43a9ba8
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2021-02-27 19:37:54 +0100

    update somethings

---
+++ helpers.js
@@ -0,0 +1,65 @@
+// dependencies
+const { get } = require("axios");
+const { load } = require("cheerio");
+
+// define variables
+const covidBaseURL = "https://disease.sh/v3/covid-19/";
+const covidURL = covidBaseURL + "countries/morocco";
+const vaccineURL = covidBaseURL + "vaccine/coverage/countries/morocco";
+const advicesURL = "http://www.covidmaroc.ma/Pages/conseilar.aspx";
+
+/**
+ * @description get covid statistics
+ */
+async function getCovid() {
+  const covidData = (await get(covidURL)).data;
+
+  // remove some props
+  delete covidData.countryInfo;
+  delete covidData.country;
+
+  // result
+  return covidData;
+}
+
+/**
+ * @description get vaccinated people
+ */
+async function getVaccine() {
+  const vaccineData = (await get(vaccineURL)).data;
+
+  // remove some props
+  delete vaccineData.country;
+
+  // result
+  return vaccineData;
+}
+
+/**
+ * @description get advices from the official gov website
+ */
+async function getAdvice() {
+  const advice = { images: [] };
+
+  const body = (await get(advicesURL)).data;
+  const $ = load(body);
+
+  JSON.parse(
+    $("body script")
+      .eq(6)
+      .html()
+      .split("pictureArray = ")[1]
+      .split(";")[0]
+      .replace(/'/g, '"')
+  ).forEach((img) => {
+    if (img) {
+      advice.images.push(img);
+    }
+  });
+
+  // result
+  return advice;
+}
+
+// exports
+module.exports = { getAdvice, getCovid, getVaccine };
